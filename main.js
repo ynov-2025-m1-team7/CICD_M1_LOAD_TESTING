119b4eff160ddb89555dc86109534837dc875bf0
AMOQUIN <augustin.moquin@ynov.com>
2025-05-06 17:00:45 +0200

    k6 test

--- main.js
+++ main.js
@@ -4,21 +4,58 @@ import { check, sleep } from "k6";
 // Test configuration
 export const options = {
   thresholds: {
-    // Assert that 99% of requests finish within 3000ms.
     http_req_duration: ["p(99) < 3000"],
+    http_req_failed: ["rate<0.01"],
   },
-  // Ramp the number of virtual users up and down
   stages: [
-    { duration: "30s", target: 15 },
-    { duration: "1m", target: 15 },
-    { duration: "20s", target: 0 },
+    { duration: "30s", target: 10 },
+    { duration: "1m", target: 50 },
+    { duration: "30s", target: 0 },
   ],
 };
 
-// Simulated user behavior
 export default function () {
   let res = http.get("https://cicdm1.onrender.com/feedbacks");
-  // Validate response status
-  check(res, { "status was 200": (r) => r.status == 200 });
+
+  // Validate response status and content
+  check(res, {
+    "status was 200": (r) => r.status === 200,
+    "response time < 2000ms": (r) => r.timings.duration < 2000,
+  });
+
   sleep(1);
+}
+
+// Stress Test
+export function stressTest() {
+  let res = http.get("https://cicdm1.onrender.com/feedbacks");
+  check(res, {
+    "status was 200": (r) => r.status === 200,
+  });
+  sleep(0.5);
+}
+
+export function spikeTest() {
+  let res = http.get("https://cicdm1.onrender.com/feedbacks");
+  check(res, {
+    "status was 200": (r) => r.status === 200,
+  });
+  sleep(0.2);
+}
+
+export function soakTest() {
+  let res = http.get("https://cicdm1.onrender.com/feedbacks");
+  check(res, {
+    "status was 200": (r) => r.status === 200,
+  });
+  sleep(1);
+}
+
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
+
+export function handleSummary(data) {
+  return {
+    "summary.json": JSON.stringify(data),
+    stdout: textSummary(data, { indent: "  " }),
+  };
 }
