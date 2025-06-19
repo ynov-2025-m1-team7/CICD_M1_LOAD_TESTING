bc422b89b8eb424b22bead16e61907cf0c820cd9
AMOQUIN <augustin.moquin@ynov.com>
2025-06-19 16:03:46 +0200

    fix

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import { check, sleep } from "k6";
 // Test configuration
 export const options = {
   thresholds: {
-    http_req_duration: ["p(99) < 3000"],
+    http_req_duration: ["p(99) < 30000"],
     http_req_failed: ["rate<0.01"],
   },
   stages: [
